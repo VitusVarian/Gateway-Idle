fa26e342420746ba106310e344277db5f24620ab
Mac Benson <xg.civwars@gmail.com>
2026-07-17 14:36:18 -0400

    Attempt 3

--- app/(game)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AppShell } from '@/components/game/app-shell'
-
-export default function GameLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <AppShell>{children}</AppShell>
-}
